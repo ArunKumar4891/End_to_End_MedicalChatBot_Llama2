20e6203c6869d50131c054ef7571c55d5ed8c5ea
Arun Kumar <arun.kumar@skybridgeinfotech.com>
2024-11-05 11:52:11 +0530

    8th commit

--- app.py
+++ app.py
@@ -1,51 +1,146 @@
+# from flask import Flask, render_template, jsonify, request
+# from src.helper import download_hugging_face_embeddings
+# from langchain_pinecone import PineconeVectorStore
+# from langchain_openai import OpenAI
+# from langchain.chains import create_retrieval_chain
+# from langchain.chains.combine_documents import create_stuff_documents_chain
+# from langchain_core.prompts import ChatPromptTemplate
+# from dotenv import load_dotenv
+# from src.prompt import *
+# import os
+
+# app = Flask(__name__)
+
+# load_dotenv()
+
+# PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+# OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
+
+# os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+# os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+# embeddings = download_hugging_face_embeddings()
+
+# index_name = "medicalchatbot4"
+
+
+# # Embed each chunk and upsert the embeddings into your Pinecone index.
+# docsearch = PineconeVectorStore.from_existing_index(
+#     index_name=index_name,
+#     embedding=embeddings
+# )
+
+
+# retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+
+
+# llm = OpenAI(temperature=0.4, max_tokens=500)
+# prompt = ChatPromptTemplate.from_messages(
+#     [
+#         ("system", system_prompt),
+#         ("human", "{input}"),
+#     ]
+# )
+
+
+# question_answer_chain = create_stuff_documents_chain(llm, prompt)
+# rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+
+# @app.route("/")
+# def index():
+#     return render_template('chat.html')
+
+# @app.route("/get", methods=["GET", "POST"])
+# def chat():
+#     msg = request.form["msg"]
+#     input = msg
+#     print(input)
+#     response = rag_chain.invoke({"input": msg})
+#     print("Response : ", response["answer"])
+#     return str(response["answer"])
+
+
+
+
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port= 8080, debug= True)
+    
+    
+    
+    
+    
+    
+    
+#########################################################################
+
+
+
+#### Llama2 Model :
+
+
 from flask import Flask, render_template, jsonify, request
 from src.helper import download_hugging_face_embeddings
 from langchain_pinecone import PineconeVectorStore
-from langchain_openai import OpenAI
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
+from langchain.llms import CTransformers
+from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
-from src.prompt import *
 import os
+from src.prompt import *
 
 app = Flask(__name__)
 
 load_dotenv()
 
-PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
-OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
-
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
+# Download embeddings
 embeddings = download_hugging_face_embeddings()
 
+# Define the index name
 index_name = "medicalchatbot4"
 
-
-# Embed each chunk and upsert the embeddings into your Pinecone index.
+# Load existing index from Pinecone
 docsearch = PineconeVectorStore.from_existing_index(
     index_name=index_name,
     embedding=embeddings
 )
 
+# Create retriever
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k": 3})
 
-retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
-
-
-llm = OpenAI(temperature=0.4, max_tokens=500)
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        ("human", "{input}"),
-    ]
+# Llama2 model setup using CTransformers
+llama2_model_path = "model/llama-2-7b-chat.ggmlv3.q4_0.bin"
+llm = CTransformers(
+    model=llama2_model_path,
+    model_type="llama",
+    config={'max_new_tokens': 512, 'temperature': 0.8}
 )
 
+# Define the prompt template for Llama2
+prompt_template = """
+Use the following pieces of information to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
-question_answer_chain = create_stuff_documents_chain(llm, prompt)
-rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+Context: {context}
+Question: {question}
 
+Only return the helpful answer below and nothing else.
+Helpful answer:
+"""
+PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+chain_type_kwargs = {"prompt": PROMPT}
+
+# Configure RetrievalQA with Llama2
+qa_chain = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=retriever,
+    return_source_documents=True,
+    chain_type_kwargs=chain_type_kwargs
+)
 
 @app.route("/")
 def index():
@@ -54,14 +149,10 @@ def index():
 @app.route("/get", methods=["GET", "POST"])
 def chat():
     msg = request.form["msg"]
-    input = msg
-    print(input)
-    response = rag_chain.invoke({"input": msg})
-    print("Response : ", response["answer"])
-    return str(response["answer"])
-
-
-
+    print("User input:", msg)
+    response = qa_chain({"query": msg})
+    print("Response:", response["result"])
+    return str(response["result"])
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
